dbf947c5d7dcff3deb2c421b8c826c20121add7f
Gustavo Pedrosa <luiz.gustavo.pedrosa@gmail.com>
2023-01-16 22:30:01 -0300

    feat: add prisma and client to project

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Heloo World!')
+import Fastify from "fastify";
+
+const app = Fastify()
+
+/** 
+ * Métodos HTTP: Get (criação), Post (criar), Put (atualizar), 
+ *    Patch (atuialização especifica), Delete (deletar)
+*/
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app.listen({port:3333}).then(()=>{
+  console.log('HTTP Server Running!')
+})
