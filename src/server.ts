a5f7a4a27de17f61ec95fdbc529d3b647be724f5
Gustavo Pedrosa <luiz.gustavo.pedrosa@gmail.com>
2023-01-16 22:46:59 -0300

    feat: create first table and add cors to project

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
 
 /** 
  * Métodos HTTP: Get (criação), Post (criar), Put (atualizar), 
  *    Patch (atuialização especifica), Delete (deletar)
 */
 
-app.get('/', () => {
-  return 'Hello World'
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
 app.listen({port:3333}).then(()=>{
